db0e09a2b1e72d8831009bfb2b0e4375d65d50a8
FrancescoZanella <francescozanella00@gmail.com>
2021-08-24 09:56:54 +0200

    home_page

--- src/code/graphics/InitialFrame.java
+++ src/code/graphics/InitialFrame.java
@@ -4,51 +4,148 @@ package graphics;
 import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 
-public class InitialFrame extends JFrame {
-        private final MyButton loginButton;
-        private MyButton registerButton;
+import static java.awt.Color.*;
+
+public class InitialFrame extends JFrame implements ActionListener {
+        private JButton homeButton;
+        private JButton teamButton;
+        private JButton leaderboardButton;
+        private JButton profileButton;
+        private JButton rulesButton;
+        private JButton settingsButton;
+        private JButton logoutButton;
 
 
-        public InitialFrame(String title) throws HeadlessException {
+        public InitialFrame(String title) {
                 super(title);
 
-               //this.setLayout(null);
-
-                BackroundPanel upPanel=new BackroundPanel("src/resources/background/banner.JPG");
-                JPanel downPanel=new JPanel();
-                loginButton=new MyButton("Login");
-                registerButton=new MyButton("Register");
-
-                //downPanel.setLayout(null);
-                upPanel.setLayout(null);
-
-                upPanel.add(loginButton);
-                upPanel.add(registerButton);
-
-
                 Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
-                double width = screenSize.getWidth();
-                double height = screenSize.getHeight();
+                int width=screenSize.width;
+                int height=screenSize.height;
 
-                upPanel.setBounds(0,0,(int) width,(int)height/5);
-                downPanel.setBounds(0,(int)height/5+1,(int)width,(int)(height-(height/5)));
-                loginButton.setBounds(upPanel.getX()+(int)width/8*6,upPanel.getY()+(int)height/10,100,20);
-                loginButton.setBackground(Color.RED);
-                loginButton.setForeground(Color.WHITE);
-
-                registerButton.setBounds(upPanel.getX()+(int)width/8*7,upPanel.getY()+(int) height/10,100,20);
-                registerButton.setBackground(Color.BLACK);
-                registerButton.setForeground(Color.WHITE);
+                //UPpanel
+                this.setLayout(null);
+                BackroundPanel upPanel=new BackroundPanel("src/resources/background/bannerslim.JPG");
+                //upPanel.setPreferredSize(new Dimension(width,100));
+                upPanel.setBounds(0,0,width,100);
                 this.add(upPanel);
+
+
+                //leftpanel
+                JPanel leftpanel=new JPanel();
+                //leftpanel.setPreferredSize(new Dimension(250,height-100));
+                leftpanel.setBackground(Color.black);
+                leftpanel.setBounds(0,100,250,height-100);
+                leftpanel.setLayout(new FlowLayout());
+                this.add(leftpanel);
+
+                //leftuppanel
+                JPanel leftuppanel=new JPanel();
+                leftuppanel.setPreferredSize(new Dimension(250,20));
+                leftuppanel.setBackground(Color.black);
+                leftpanel.add(leftuppanel);
+
+                //HomeButton
+                homeButton=new JButton("Home",new ImageIcon(Objects.requireNonNull(getClass().getResource("/icons/icons8_home_20px_7.png"))));
+                homeButton.setPreferredSize(new Dimension(200,40));
+                homeButton.setForeground(white);
+                homeButton.setBackground(Color.black);
+                homeButton.setBorderPainted(false);
+                homeButton.setFont(new Font("Comic Sans MS", Font.BOLD, 15));
+                homeButton.setHorizontalAlignment(SwingConstants.LEFT);
+                leftpanel.add(homeButton);
+
+                //TeamButton
+                teamButton=new JButton("Team",new ImageIcon(Objects.requireNonNull(getClass().getResource("/icons/icons8_crowd_20px.png"))));
+                teamButton.setPreferredSize(new Dimension(200,40));
+                teamButton.setForeground(white);
+                teamButton.setBackground(Color.black);
+                teamButton.setBorderPainted(false);
+                teamButton.setFont(new Font("Comic Sans MS", Font.BOLD, 15));
+                teamButton.setHorizontalAlignment(SwingConstants.LEFT);
+                leftpanel.add(teamButton);
+
+                //leaderboards botton
+                leaderboardButton=new JButton("Leaderboards",new ImageIcon(Objects.requireNonNull(getClass().getResource("/icons/icons8_leaderboard_20px.png"))));
+                leaderboardButton.setPreferredSize(new Dimension(200,40));
+                leaderboardButton.setForeground(white);
+                leaderboardButton.setBackground(Color.black);
+                leaderboardButton.setBorderPainted(false);
+                leaderboardButton.setFont(new Font("Comic Sans MS", Font.BOLD, 15));
+                leaderboardButton.setHorizontalAlignment(SwingConstants.LEFT);
+                leftpanel.add(leaderboardButton);
+                
+                //profile botton
+                profileButton=new JButton("Profile",new ImageIcon(Objects.requireNonNull(getClass().getResource("/icons/icons8_customer_20px.png"))));
+                profileButton.setPreferredSize(new Dimension(200,40));
+                profileButton.setForeground(white);
+                profileButton.setBackground(Color.black);
+                profileButton.setBorderPainted(false);
+                profileButton.setFont(new Font("Comic Sans MS", Font.BOLD, 15));
+                profileButton.setHorizontalAlignment(SwingConstants.LEFT);
+                leftpanel.add(profileButton);
+
+                //rules botton
+                rulesButton=new JButton("Rules",new ImageIcon(Objects.requireNonNull(getClass().getResource("/icons/regole.png"))));
+                rulesButton.setPreferredSize(new Dimension(200,40));
+                rulesButton.setForeground(white);
+                rulesButton.setBackground(Color.black);
+                rulesButton.setBorderPainted(false);
+                rulesButton.setFont(new Font("Comic Sans MS", Font.BOLD, 15));
+                rulesButton.setHorizontalAlignment(SwingConstants.LEFT);
+                leftpanel.add(rulesButton);
+
+
+                //leftdownpanel
+                JPanel leftdownpanel=new JPanel();
+                leftdownpanel.setPreferredSize(new Dimension(250,230));
+                leftdownpanel.setBackground(black);
+                leftpanel.add(leftdownpanel);
+
+                //barseparator
+                JSeparator sep=new JSeparator();
+                sep.setBackground(WHITE);
+                sep.setForeground(WHITE);
+                sep.setOrientation(SwingConstants.HORIZONTAL);
+                sep.setPreferredSize(new Dimension(200,10));
+                leftdownpanel.add(sep);
+                
+                //settings button
+                settingsButton=new JButton("Settings",new ImageIcon(Objects.requireNonNull(getClass().getResource("/icons/settings.png"))));
+                settingsButton.setPreferredSize(new Dimension(200,40));
+                settingsButton.setForeground(white);
+                settingsButton.setBackground(Color.black);
+                settingsButton.setBorderPainted(false);
+                settingsButton.setFont(new Font("Comic Sans MS", Font.BOLD, 15));
+                settingsButton.setHorizontalAlignment(SwingConstants.LEFT);
+                leftpanel.add(settingsButton);
+
+                //logout button
+                logoutButton=new JButton("Logout",new ImageIcon(Objects.requireNonNull(getClass().getResource("/icons/logout.png"))));
+                logoutButton.setPreferredSize(new Dimension(200,40));
+                logoutButton.setForeground(white);
+                logoutButton.setBackground(Color.black);
+                logoutButton.setBorderPainted(false);
+                logoutButton.setFont(new Font("Comic Sans MS", Font.BOLD, 15));
+                logoutButton.setHorizontalAlignment(SwingConstants.LEFT);
+                leftpanel.add(logoutButton);
+
+
+                //downpanel
+                JPanel downPanel=new JPanel();
+                downPanel.setBackground(white);
+                downPanel.setBounds(250,500,width-250,200);
                 this.add(downPanel);
 
-
+                
 
                 List <Image> listImage=new ArrayList<>();
                 try {
@@ -61,8 +158,10 @@ public class InitialFrame extends JFrame {
                 }
 
 
+                //frame
+                this.add(upPanel,BorderLayout.NORTH);
+                this.setExtendedState(JFrame.MAXIMIZED_BOTH);
                 this.setIconImages(listImage);
-                this.setSize((int)width,(int)height);
                 this.setResizable(false);
                 this.setVisible(true);
 
@@ -75,4 +174,8 @@ public class InitialFrame extends JFrame {
         }
 
 
+        @Override
+        public void actionPerformed(ActionEvent e) {
+
+        }
 }
